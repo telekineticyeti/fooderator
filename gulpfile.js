1077b2d146f880e67ffeebd598dc7758f380088c
Paul Castle <paul.casltle@gmail.com>
2017-12-20 14:02:15 +0000

    More development tools added for GUI development

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+var sass_options = {
+	outputStyle: 'compressed',
+	includePaths: [
+		require('node-bourbon').includePaths
+	]
+}
+gulp.task('sass', function () {
+	return gulp
+		.src('./sass/**/*.s*ss')
+		.pipe(sass(sass_options).on("error", sass.logError))
+		.pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('autoprefix', () =>
+	gulp.src('src/**/*.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('public'))
+);
+
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.s*ss', ['sass']);
+	gulp.watch('src/**/*.css', ['autoprefix']);
+});
+
+gulp.task('default', ['watch']);
