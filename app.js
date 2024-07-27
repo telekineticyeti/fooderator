92c58390efe28fb405ff410cf22ecb2855e5cb39
Paul Castle <paul.castle@arria.com>
2024-07-27 01:30:27 +0100

    Pushing an old update

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const ejs = require('ejs');
 const moment = require('moment');
 
-// Database 
+// Database
 const connection_string = process.env.DATABASE_URL;
 const promise = require('bluebird');
 const pgp = require('pg-promise')({ promiseLib: promise });
@@ -19,118 +19,134 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride());
 
 app.listen(80, () => {
-	console.log('Listening on http://localhost:8010');
+  console.log('Listening on http://localhost:8010');
 });
 
 app.get('/', (req, res, next) => {
-	let result_object = [];
-	let db = pgp(connection_string);
+  let result_object = [];
+  let db = pgp(connection_string);
 
-	db.any('select * from meals', [true])
-		.then(data => {
-			res.render('pages/home', {
-				meals: data
-			});
-		})
-		.catch(error => {
-			console.log('ERROR:', error);
-		})
-		.finally(db.$pool.end);
+  db.any('select * from meals', [true])
+    .then((data) => {
+      res.render('pages/home', {
+        meals: data,
+      });
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    })
+    .finally(db.$pool.end);
 });
 
-app.get('/api/meals', function(req, res) {
-	let result_object = [];
-	let db = pgp(connection_string);
+app.get('/api/meals', function (req, res) {
+  let result_object = [];
+  let db = pgp(connection_string);
 
-	db.any('select * from meals', [true])
-		.then(data => {
-			res.json({ status: 'success', data });
-		})
-		.catch(error => {
-			console.log('ERROR:', error);
-		})
-		.finally(db.$pool.end);
+  db.any('select * from meals', [true])
+    .then((data) => {
+      res.json({ status: 'success', data });
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    })
+    .finally(db.$pool.end);
 });
-
 
 /**
  * Add a new meal to the database using PUT method
  */
-app.put('/api/meals', function(req, res) {
-	let name = req.body.name,
-		description = req.body.description;
+app.put('/api/meals', function (req, res) {
+  let name = req.body.name,
+    description = req.body.description;
 
-	let db = pgp(connection_string);
-	db.any('INSERT INTO meals(name, description) values($1, $2)', [name, description])
-		.then(data => {
-			res.json({ message: name + ' was added to your meal list!' });
-		})
-		.catch(error => {
-			res.json({ status: 500, statusText: error });
-		})
-		.finally(db.$pool.end);
+  let db = pgp(connection_string);
+  db.any('INSERT INTO meals(name, description) values($1, $2)', [
+    name,
+    description,
+  ])
+    .then((data) => {
+      res.json({ message: name + ' was added to your meal list!' });
+    })
+    .catch((error) => {
+      res.json({ status: 500, statusText: error });
+    })
+    .finally(db.$pool.end);
 });
-
 
 /**
  * Update a meal record's information
  */
-app.post('/api/meals/:id', function(req, res) {
-	let db = pgp(connection_string);
+app.post('/api/meals/:id', function (req, res) {
+  let db = pgp(connection_string);
 
-	const id = req.params.id;
-	const updated = moment().format('YYYY-MM-DD HH:mm:ss');
+  const id = req.params.id;
+  const updated = moment().format('YYYY-MM-DD HH:mm:ss');
 
-	// Queue up Database transactions
-	db.tx(t => {
-		var transactions = [];
+  // Queue up Database transactions
+  db.tx((t) => {
+    var transactions = [];
 
-		if (typeof(req.body.pending) !== "undefined" && typeof(req.body.pending) === "boolean") {
-			console.log(req.body.pending);
-			var meal_pending = t.none('update meals SET pending = $1 WHERE id = $2', [req.body.pending, id]);
-			transactions.push(meal_pending);
-		}
+    if (
+      typeof req.body.pending !== 'undefined' &&
+      typeof req.body.pending === 'boolean'
+    ) {
+      console.log(req.body.pending);
+      var meal_pending = t.none('update meals SET pending = $1 WHERE id = $2', [
+        req.body.pending,
+        id,
+      ]);
+      transactions.push(meal_pending);
+    }
 
-		if (req.body.name) {
-			var meal_name = t.none('update meals SET name = $1 WHERE id = $2', [req.body.name, id]);
-			transactions.push(meal_name);
-		}
+    if (req.body.name) {
+      var meal_name = t.none('update meals SET name = $1 WHERE id = $2', [
+        req.body.name,
+        id,
+      ]);
+      transactions.push(meal_name);
+    }
 
-		if (req.body.description) {
-			var meal_description = t.none('update meals SET description = $1 WHERE id = $2', [req.body.description, id]);
-			transactions.push(meal_description);
-		}
+    if (req.body.description) {
+      var meal_description = t.none(
+        'update meals SET description = $1 WHERE id = $2',
+        [req.body.description, id]
+      );
+      transactions.push(meal_description);
+    }
 
-		// Set row updated date/time
-		var meal_updated = t.none('update meals SET updated = $1 WHERE id = $2', [updated, id]);
-		transactions.push(meal_updated);
+    // Set row updated date/time
+    var meal_updated = t.none('update meals SET updated = $1 WHERE id = $2', [
+      updated,
+      id,
+    ]);
+    transactions.push(meal_updated);
 
-		// Resolve all queries
-		return t.batch(transactions);
-	})
-	.then(data => {
-		res.json({ status: 'success', data });
-	})
-	.catch(error => {
-		console.log('ERROR:', error);
-	})
-	.finally(db.$pool.end);
+    // Resolve all queries
+    return t.batch(transactions);
+  })
+    .then((data) => {
+      res.json({ status: 'success', data });
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    })
+    .finally(db.$pool.end);
 });
 
 app.get('/ingredients', (req, res, next) => {
-	res.render('pages/ingredients');
+  res.render('pages/ingredients');
 });
 
-app.get('/api/ingredients', function(req, res) {
-	let result_object = [];
-	let db = pgp(connection_string);
+app.get('/api/ingredients', function (req, res) {
+  let result_object = [];
+  let db = pgp(connection_string);
 
-	db.any('select * from ingredients', [true])
-		.then(data => {
-			res.json({ status: 'success', data });
-		})
-		.catch(error => {
-			console.log('ERROR:', error);
-		})
-		.finally(db.$pool.end);
+  db.any('select * from ingredients', [true])
+    .then((data) => {
+      res.json({ status: 'success', data });
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    })
+    .finally(db.$pool.end);
 });
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,157 +1,168 @@
-var app = angular.module('fooderator', ['ngMaterial']).config(function($mdThemingProvider) {
-	$mdThemingProvider.theme('default')
-	.primaryPalette('green')
-	.accentPalette('orange');
-});
+var app = angular
+  .module('fooderator', ['ngMaterial'])
+  .config(function ($mdThemingProvider) {
+    $mdThemingProvider
+      .theme('default')
+      .primaryPalette('green')
+      .accentPalette('orange');
+  });
 
-app.controller('ingredients', function($scope, $http, $mdToast, $mdDialog, $log) {
-	/**
-	 * Load the ingredient list
-	 */
-	$scope.load_ingredient_list = function () {
-		$http.get('/api/ingredients')
-			.then(response => {
-				if (response.status === 200)  {
-					$scope.ingredients = response.data.data;
-				} else {
-					return false
-				}
-			});
-			self.new_ingredient = function(chip) {
-				return {
-					name: chip,
-					type: 'unknown'
-				};
-			};
-	};
-});
+app.controller(
+  'ingredients',
+  function ($scope, $http, $mdToast, $mdDialog, $log) {
+    /**
+     * Load the ingredient list
+     */
+    $scope.load_ingredient_list = function () {
+      $http.get('/api/ingredients').then((response) => {
+        if (response.status === 200) {
+          $scope.ingredients = response.data.data;
+        } else {
+          return false;
+        }
+      });
+      self.new_ingredient = function (chip) {
+        return {
+          name: chip,
+          type: 'unknown',
+        };
+      };
+    };
+  }
+);
 
+app.controller('meals', function ($scope, $http, $mdToast, $mdDialog, $log) {
+  $scope.snackbar = function (message) {
+    $mdToast.show(
+      $mdToast
+        .simple()
+        .textContent(message)
+        .position('bottom right')
+        .hideDelay(3000)
+    );
+  };
 
-app.controller('meals', function($scope, $http, $mdToast, $mdDialog, $log) {
-	$scope.snackbar = function(message) {
-		$mdToast.show(
-			$mdToast.simple().textContent(message).position('bottom right').hideDelay(3000)
-		)
-	};
+  /**
+   * Load the list of meals to be displayed
+   */
+  $scope.load_meal_list = function () {
+    $http.get('/api/meals').then((response) => {
+      if (response.status === 200) {
+        $scope.meals = response.data.data;
+      } else {
+        return false;
+      }
+    });
+  };
 
+  /**
+   * Handle meal click event (set meal pending status)
+   */
+  $scope.meal_click = function (id, status, name) {
+    let update_data = JSON.stringify({
+      pending: status,
+    });
 
-	/**
-	 * Load the list of meals to be displayed
-	 */
-	$scope.load_meal_list = function () {
-		$http.get('/api/meals')
-			.then(response => {
-				if (response.status === 200)  {
-					$scope.meals = response.data.data;
-				} else {
-					return false
-				}
-			});
-	};
+    $http
+      .post('/api/meals/' + id, update_data)
+      .then((response) => {
+        $scope.snackbar(name + ' has been updated');
+      })
+      .catch((error) => {
+        $scope.snackbar('Error: ' + error);
+      });
+  };
 
+  /**
+   * Initiate add meal dialog
+   */
+  $scope.meal_add = function (id, name, description) {
+    $mdDialog
+      .show({
+        controller: add_meal_dialog_controller,
+        templateUrl: '/templates/add_meal_dialog.html',
+        parent: angular.element(document.body),
+        clickOutsideToClose: true,
+        fullscreen: $scope.customFullscreen,
+      })
+      .then(function (data) {
+        $scope.snackbar('Meal successfully added');
+        $scope.load_meal_list();
+      });
 
-	/**
-	 * Handle meal click event (set meal pending status)
-	 */
-	$scope.meal_click = function(id, status, name) {
-		let update_data = JSON.stringify({
-			pending: status
-		});
+    function add_meal_dialog_controller($scope, $mdDialog, $log) {
+      $scope.cancel = function () {
+        $mdDialog.cancel();
+      };
+      $scope.save = function (answer) {
+        if ($scope.meal_name) {
+          var data = JSON.stringify({
+            name: $scope.meal_name,
+            description: $scope.meal_description,
+          });
+          $http
+            .put('/api/meals', data)
+            .then((response) => {
+              $mdDialog.hide();
+            })
+            .catch((error) => {
+              $log.log(error);
+            });
+        }
+      };
+    }
+  };
 
-		$http.post('/api/meals/' + id, update_data)
-			.then(response => {
-				$scope.snackbar(name + ' has been updated');
-			}).catch(error => {
-				$scope.snackbar('Error: ' + error);
-			});
-	}
+  /**
+   * Initiate meal editing dialog
+   */
+  $scope.meal_edit = function (id, name, description) {
+    var meal_data = {
+      id: id,
+      name: name,
+      description: description,
+    };
 
+    $mdDialog
+      .show({
+        locals: { data: meal_data },
+        controller: edit_meal_dialog_controller,
+        templateUrl: '/templates/edit_meal_dialog.html',
+        parent: angular.element(document.body),
+        targetEvent: id,
+        clickOutsideToClose: true,
+        fullscreen: $scope.customFullscreen,
+      })
+      .then(function (answer) {
+        $scope.snackbar('Meal successfully updated');
+        $scope.load_meal_list();
+      });
 
-	/**
-	 * Initiate add meal dialog
-	 */
-	$scope.meal_add = function(id, name, description) {
-		$mdDialog.show({
-			controller: add_meal_dialog_controller,
-			templateUrl: '/templates/add_meal_dialog.html',
-			parent: angular.element(document.body),
-			clickOutsideToClose: true,
-			fullscreen: $scope.customFullscreen
-		})
-		.then(function(data) {
-			$scope.snackbar('Meal successfully added');
-			$scope.load_meal_list();
-		});
+    function edit_meal_dialog_controller($scope, $mdDialog, $log, data) {
+      $scope.dialog_data = data;
 
-		function add_meal_dialog_controller($scope, $mdDialog, $log) {
-			$scope.cancel = function() {
-				$mdDialog.cancel();
-			};
-			$scope.save = function(answer) {
-				if ($scope.meal_name) {
-					var data = JSON.stringify({
-						name: $scope.meal_name,
-						description: $scope.meal_description
-					});
-					$http.put('/api/meals', data)
-						.then(response => {
-							$mdDialog.hide();
-						}).catch(error => {
-							$log.log(error)
-						});
-				}
-			};
-		};
-	}
+      $scope.hide = function () {
+        $mdDialog.hide();
+      };
+      $scope.cancel = function () {
+        $mdDialog.cancel();
+      };
 
+      $scope.save = function (answer) {
+        let update_data = JSON.stringify({
+          name: $scope.dialog_data.name,
+          description: $scope.dialog_data.description,
+        });
 
-	/**
-	 * Initiate meal editing dialog
-	 */
-	$scope.meal_edit = function(id, name, description) {
-		var meal_data = {
-			id: id,
-			name: name,
-			description: description
-		};
-
-		$mdDialog.show({
-			locals: { data: meal_data },
-			controller: edit_meal_dialog_controller,
-			templateUrl: '/templates/edit_meal_dialog.html',
-			parent: angular.element(document.body),
-			targetEvent: id,
-			clickOutsideToClose: true,
-			fullscreen: $scope.customFullscreen
-		})
-		.then(function(answer) {
-			$scope.snackbar('Meal successfully updated');
-			$scope.load_meal_list();
-		});
-
-		function edit_meal_dialog_controller($scope, $mdDialog, $log, data) {
-			$scope.dialog_data = data;
-
-			$scope.hide = function() {
-				$mdDialog.hide();
-			};
-			$scope.cancel = function() {
-				$mdDialog.cancel();
-			};
-
-			$scope.save = function(answer) {
-				let update_data = JSON.stringify({
-					name: $scope.dialog_data.name,
-					description: $scope.dialog_data.description
-				});
-
-				$http.post('/api/meals/' +  $scope.dialog_data.id, update_data)
-					.then(response => {
-						$mdDialog.hide();
-					}).catch(error => {
-						$log.log(error)
-					});
-			};
-		}
-	}
+        $http
+          .post('/api/meals/' + $scope.dialog_data.id, update_data)
+          .then((response) => {
+            $mdDialog.hide();
+          })
+          .catch((error) => {
+            $log.log(error);
+          });
+      };
+    }
+  };
 });
